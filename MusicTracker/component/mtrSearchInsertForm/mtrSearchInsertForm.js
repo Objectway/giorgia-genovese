c1f55ef88925479965f153d99352504b05aecafa
giorgaigenovese <ggiorgia.genovese@gmail.com>
2019-05-13 16:36:11 +0200

    step 5

--- MusicTracker/component/mtrSearchInsertForm/mtrSearchInsertForm.js
+++ MusicTracker/component/mtrSearchInsertForm/mtrSearchInsertForm.js
@@ -1,29 +1,34 @@
 myApp
     .component('mtrSearchInsertForm',{
+        binding:{
+            album:'='
+        },
         controller:function($scope, mtrServiceList,mtrSearchService){
             var ctrl=this;
-            
+            ctrl.searchResult=[];
+            ctrl.selectedItem=null;
             ctrl.salva=function(){
                 let newItem= {
-                    artista: $scope.artista,
-                    album: $scope.album,
-                    trecce: $scope.tracce,
-                    prezzo: $scope.prezzo,
+                    artista: ctrl.selectedItem.artistName,
+                    artistaId: ctrl.selectedItem.artistId,
+                    album: ctrl.selectedItem.album,
+                    trecce: ctrl.selectedItem.trackCount,
+                    prezzo: ctrl.selectedItem.collectionPrice,
                     punteggio: ctrl.punteggio
                 }
                 mtrServiceList.addItemToList(newItem);
-                console.log(punteggio)
+                //console.log(newItem)
                 
             }
             ctrl.search=function(){
-                mtrSearchService.searchForTitle($scope.album)
+                mtrSearchService.searchForTitle(ctrl.album)
                 //console.log(ctrl.album)
                 .then(result=>{
                     ctrl.searchResult=result;
-                    console.log(result)
+                    console.log(ctrl.searchResult)
                 })
                 .catch(error=>{
-                    console.log(error)
+                    //console.log(error)
                 })
             }
         },
--- MusicTracker/services/mtrSearchService.js
+++ MusicTracker/services/mtrSearchService.js
@@ -1,17 +1,35 @@
 myApp
     .factory('mtrSearchService',['$http', '$q', function($http, $q){ 
-        const titolo='Poetica';
-        const endpoint= 'https://itunes.apple.com/search?entity=album&term='+titolo;
+        
+        const endpoint= 'https://itunes.apple.com/search?entity=album&term=';
         return{
             searchForTitle: function( searchString){
                 let deferred= $q.defer();
-                    let searchEndpoint= endpoint+'&s='+searchString;                                   
+                    let searchEndpoint= endpoint+searchString;                                   
                         $http({
                             method:'GET',
                             url:searchEndpoint
                             }).then(function(responseJson){
                                 if(responseJson.data.result!==null){
-                                    deferred.resolve(responseJson.data.result)
+                                    deferred.resolve(responseJson.data.results)
+                                }else{
+                                    deferred.resolve(responseJson.data.Error)
+                                }  
+                                //console.log(responseJson)  
+                            }).catch(function(error){
+                                deferred.reject(error)
+                            }) 
+                    return deferred.promise;
+            },
+            getFilmById:function(imdbID){
+                let deferred= $q.defer();
+                    let searchEndpoint= endpoint+'&i='+imdbID;                                   
+                        $http({
+                            method:'GET',
+                            url:searchEndpoint
+                            }).then(function(responseJson){
+                                if(responseJson.data.result!==null){
+                                    deferred.resolve(responseJson.data.results)
                                 }else{
                                     deferred.resolve(responseJson.data.Error)
                                 }  
@@ -20,23 +38,6 @@ myApp
                                 deferred.reject(error)
                             }) 
                     return deferred.promise;
-            },
-         /*   getFilmById:function(imdbID){
-                let deferred= $q.defer();
-                    let searchEndpoint= endpoint+'&i='+imdbID;                                   
-                        $http({
-                            method:'GET',
-                            url:searchEndpoint
-                            }).then(function(responseJson){
-                                if(responseJson.data.Response==='True'){
-                                    deferred.resolve(responseJson.data)
-                                }else{
-                                    deferred.resolve(responseJson.data.Error)
-                                }    
-                            }).catch(function(error){
-                                deferred.reject(error)
-                            }) 
-                    return deferred.promise;
-            }*/
+            }
         }
 }])  
