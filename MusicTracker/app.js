94dae80b013998ead8c0d4bb32398ef6620ad487
giorgaigenovese <ggiorgia.genovese@gmail.com>
2019-05-13 17:06:47 +0200

    step 6

--- MusicTracker/app.js
+++ MusicTracker/app.js
@@ -1,1 +1,18 @@
 let myApp=angular.module('musicTracker',['ui.router']);
+myApp.config(function($stateProvider){
+    let routes=[
+        {
+            name:'aggiungi',
+            url: '/aggiungi',
+            component: 'mtrSearchInsertForm'
+        },
+        {
+            name:'list',
+            url: '/list',
+            component: 'mtrManualList'
+        }      
+    ];
+    routes.forEach(r =>{
+        $stateProvider.state(r);
+    })
+})
